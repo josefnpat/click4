5f45907dc4f61028568af366e9af86fd85facc0d
Seppi <josefnpat@gmail.com>
2022-02-22 09:43:15 -0600

    Reworking QSOUND so it works on time and in order

--- src/qsound.lua
+++ src/qsound.lua
@@ -3,9 +3,14 @@ local qs = {}
 function qs.new(init)
   init = init or {}
   local self = {}
+
   self.queue = {}
   self.enqueue = qs.enqueue
   self.update = qs.update
+
+  self.playdt = 0
+  self.playt = 0.5
+
   return self
 end
 
@@ -14,12 +19,17 @@ function qs:enqueue(audio)
 end
 
 function qs:update(dt)
-  if self.last_sound and not self.last_sound:isPlaying() then
-    self.last_sound = nil
+  if self.current_sound == nil and #self.queue > 0 then
+    self.current_sound = table.remove(self.queue,1)
+    self.current_sound:play()
   end
-  if #self.queue > 0 and not self.last_sound then
-    self.last_sound = table.remove(self.queue)
-    self.last_sound:play()
+  if self.current_sound and self.current_sound:isPlaying() then
+    self.playdt = self.playdt + dt
+    if self.playdt >= self.playt then
+      self.playdt = self.playdt - self.playt
+      self.current_sound:stop()
+      self.current_sound = nil
+    end
   end
 end
 
